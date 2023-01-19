8ff75842c6542b71058abceef657987c1e87fdf8
Muhammad hafid afandi <60306984+MHafidafandi@users.noreply.github.com>
2023-01-19 22:44:47 +0700

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = require("./routes/toDoRoute");
 const cors = require("cors");
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.port || 4040;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
